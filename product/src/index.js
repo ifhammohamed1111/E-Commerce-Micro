98b47e6cb3b37088be2204dd416ce2698a0da49a
Mohamed MRI <mohamedmri.21@uom.lk>
2024-03-21 20:59:11 +0530

    Update response message in index.js

--- product/src/index.js
+++ product/src/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/", (req, res, next) => {
-  return res.status(200).json({ msg: "Hello from Customer" });
+  return res.status(200).json({ msg: "Hello from Products" });
 });
 
 app.listen(1020, () => {
