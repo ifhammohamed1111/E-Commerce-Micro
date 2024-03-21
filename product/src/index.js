a66535c5a3f757965ece53898ace635dc263a7f3
Mohamed MRI <mohamedmri.21@uom.lk>
2024-03-21 21:04:44 +0530

    Update response message and server log

--- product/src/index.js
+++ product/src/index.js
@@ -5,9 +5,9 @@ const app = express();
 app.use(express.json());
 
 app.use("/", (req, res, next) => {
-  return res.status(200).json({ msg: "Hello from Products" });
+  return res.status(200).json({ msg: "Hello from Products Service" });
 });
 
 app.listen(1020, () => {
-  console.log("Server is running on port 1020");
+  console.log("Server is running on port 1020 Products Service");
 });
