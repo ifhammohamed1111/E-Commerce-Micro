12862a8081509d6b0c3b1b804c0998dd87e7b05e
Mohamed MRI <mohamedmri.21@uom.lk>
2024-03-22 00:06:26 +0530

    Add JSON parsing and dynamic port assignment

--- product/src/index.js
+++ product/src/index.js
@@ -2,12 +2,16 @@ const express = require("express");
 
 const app = express();
 
+// parse requests of content-type - application/json
 app.use(express.json());
 
+// simple route
 app.use("/", (req, res, next) => {
   return res.status(200).json({ msg: "Hello from Products Service" });
 });
 
-app.listen(1020, () => {
+// set port, listen for requests
+const PORT = process.env.PORT || 1020;
+app.listen(PORT, () => {
   console.log("Server is running on port 1020 Products Service");
 });
